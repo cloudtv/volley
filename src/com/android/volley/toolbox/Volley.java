ada92987263ec88b7e6d712afc5d95e54504f93c
ajit gevariya <gevariyaajit@gmail.com>
2013-10-02 04:53:40 +0530

    minor changes

--- src/com/android/volley/toolbox/Volley.java
+++ src/com/android/volley/toolbox/Volley.java
@@ -7,14 +7,20 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.net.http.AndroidHttpClient;
 import android.os.Build;
+import android.os.Environment;
 
 import com.android.volley.LocalEnabledNetwork;
 import com.android.volley.Network;
 import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HttpClientStack;
+import com.android.volley.toolbox.HttpStack;
+import com.android.volley.toolbox.HurlStack;
 
 public class Volley
 {
-	public RequestQueue newRequestQueue(Context context, HttpStack stack, File cacheDir, int diskUsageBites) {
+	public RequestQueue newRequestQueue(Context context, HttpStack stack, String diskPath, int diskUsageBites) {
+		File cacheDir = getCacheDir(context, diskPath);
 
 		String userAgent = "";
 		try {
@@ -45,6 +51,18 @@ public class Volley
 		RequestQueue queue = new RequestQueue(diskBaseCache, network);
 		queue.start();
 		return queue;
+	}
+
+	public File getCacheDir(Context context, String path) {
+		File cacheDir;
+		// Find the dir to save cached images
+		if(android.os.Environment.getExternalStorageState().equals(android.os.Environment.MEDIA_MOUNTED))
+			cacheDir = new File(Environment.getExternalStorageDirectory(), path);
+		else
+			cacheDir = context.getCacheDir();
+		if(!cacheDir.exists())
+			cacheDir.mkdirs();
+		return cacheDir;
 	}
 
 }
